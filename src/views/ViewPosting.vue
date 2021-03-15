fa371570102ae5b6a09c09d5e3f1cbdb44a78cd9
Yiming Zhong <ym.zhong@mail.utoronto.ca>
2021-03-16 00:09:36 +0800

    more details in single post viewing

--- src/views/ViewPosting.vue
+++ src/views/ViewPosting.vue
@@ -1,16 +1,40 @@
 <template>
   <v-container>
-    <div v-if="$currentUser">
-      <p class="subtitle"> Welcome, {{ urlId }}. </p>
-            <p class="subtitle" v-if="userPost"> Welcome, {{ userPost['description'] }}. </p>
 
-    </div>
+<div v-if="$currentUser && userPost">
+        
+      <v-banner  class="subtitle text-h6 text-center"> Welcome, this is user post {{ urlId }}. </v-banner>
+               
+  <v-card
+    class="mx-auto mt-5"
+    max-width="80%"
+  >
+    <v-card-text>
+      <div>post id: {{ urlId}}</div>
+      <p class="display-1 text--primary">
+          {{userPost['title']}}
+      </p>
+      <div class="text--primary">
+        <div v-html="userPost['description']"> </div>
+      </div>
+    </v-card-text>
+  
+      <v-card-actions>
+      <v-btn
+        color="deep-purple lighten-2"
+        text
+      >
+        More action
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
     
-    <br/>
+</div>
 
-    <p>Home page.</p>
 
   </v-container>
+
 </template>
 
 <script lang="ts">
@@ -29,7 +53,7 @@ export default {
         async viewPosting() {
             
             let userPost = await this.axios.get('/userPostings/' + this.urlId);
-            console.log(userPost);
+            console.log(userPost.data);
             this.userPost = userPost.data;
         },  
     },
