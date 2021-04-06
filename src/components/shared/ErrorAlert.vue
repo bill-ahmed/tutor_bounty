a459f958ff816476644cbe00706b06f26eb9a887
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2021-04-06 11:37:49 -0400

    animate v-alert

--- src/components/shared/ErrorAlert.vue
+++ src/components/shared/ErrorAlert.vue
@@ -2,7 +2,7 @@
   <v-row justify="center">
     <v-spacer v-if="!fullWidth"/>
     <v-col>
-      <v-alert v-for="err in errors" :key="(err.msg || err.message)" elevation="4" dismissible type="error" min-width="350px" :max-width="fullWidth ? '' : '700px'">
+      <v-alert v-for="err in errors" :key="(err.msg || err.message)" transition="scroll-x-transition" elevation="4" dismissible type="error" min-width="350px" :max-width="fullWidth ? '' : '700px'">
         {{ err.msg || err.message }}
       </v-alert>
     </v-col>
